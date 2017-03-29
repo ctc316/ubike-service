9a5b4a4f2892df957d4e267867e91bb2e75b2bb3
ctc316 <mike.tc.chen101@gmail.com>
2017-03-29 23:00:18 +0800

    working swagger: document, validation; find2Nearest doc done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const Koa = require('koa')
 const Router = require('koa-router')
-const bodyParser = require('koa-bodyparser')
+const body = require('koa-body')
 const helmet = require('koa-helmet')
 const cors = require('kcors')
 
@@ -18,21 +18,32 @@ const config = require(path.join(__dirname, "config"))
 let app = new Koa()
 
 //Body parser
-app.use(bodyParser())
+app.use(body())
 
 //CORS
-app.use(cors());
+app.use(cors())
 
 //Security
 app.use(helmet())
 
 //Log
 app.use(async (ctx, next) => {
-	const start = new Date();
-	await next();
-	const ms = new Date() - start;
-	console.log(`-> ${ctx.method} ${ctx.url} - ${ms}ms`);
+	const start = new Date()
+	await next()
+	const ms = new Date() - start
+	console.log(`${ctx.request.ip} -> ${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+// Swagger - UI
+const document = swagger.loadDocumentSync(__dirname + '/swagger.yml')
+app.use(swagger_ui(document, "/docs"))
+
+//Swagger - validate
+if (!swagger.validateDocument(document)) {
+  throw Error(`./swagger.yml does not conform to the Swagger 2.0 schema`)
+}
+app.use(swagger_validate(document))
+
 
 // Config Routes
 fs.readdir(config.path.routes, (err, files) => {
@@ -43,33 +54,10 @@ fs.readdir(config.path.routes, (err, files) => {
 		});
 })
 
-//Swagger - UI
-const document = swagger.loadDocumentSync(__dirname + '/swagger.yml');
-app.use(swagger_ui(document, "/docs"));
-
-//Swagger validate document
-// if (!swagger.validateDocument(document)) {
-//   throw Error(`./swagger.yml does not conform to the Swagger 2.0 schema`);
-// }
-// // app.use(body());
-// app.use(swagger_validate(document));
-
-// var router = swagger_router(document);
-// router.get('/ping', async (context) => {
-//   context.status = 200;
-//   context.body = {
-//     serverTime: new Date().toISOString()
-//   }
-// })
-
-// router.app()         // get the koa 2 server
-//   .use(swagger_ui(document, "/docs")) // only needed if you want to publish a swagger-ui for the API
-//   .listen(3000);     // start handling requests on port 3000
-
-//Listening
+// Listening
 app.listen(config.port, () => {
 		console.log(`[ ${package.name} v${package.version} ] started, listening on port ${config.port}...", `)
 		console.info(config)
-});
+})
 
 
--- config/routes/v1.js
+++ config/routes/v1.js
@@ -8,12 +8,14 @@ const Router = require('koa-router')
 module.exports = function(app){
     let v1 = new Router({ prefix: '/v1' })
 		.get('/', function (ctx, next) {
-	    	ctx.body = `v1: Hello ${ctx.request.query.name}!`;
+			ctx.body = `v1: Hello ${ctx.request.query.name}!`
 		})
 		.post('/', function (ctx, next) {
-	    	ctx.body  = {version: "v1", route: '/', query: ctx.request.query, body: ctx.request.body};
+	    	ctx.body  = {version: "v1", route: '/', query: ctx.request.query, body: ctx.request.body}
 		})
-
+		.get('/ubike-station/taipei', function (ctx, next) {
+			ctx.body = `v1: lat=${ctx.request.query.lat}, lng=${ctx.request.query.lng}`
+		})
 	app.use(v1.routes())
 	app.use(v1.allowedMethods())
 };
